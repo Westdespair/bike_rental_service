adaba423c873c67c47fff3269c794cb114e9dc95
Sindre Skorpen <64530905+Westdespair@users.noreply.github.com>
2022-02-10 12:23:18 +0100

    Added initialized springboot project
    Added initialized Springboot 2.6.3 project.
    -Java 17
    -Maven
    -Spring web dependency

---
+++ bikerental/src/main/java/no/ntnu/bikerental/BikerentalApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.bikerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikerentalApplication.class, args);
+	}
+
+}
