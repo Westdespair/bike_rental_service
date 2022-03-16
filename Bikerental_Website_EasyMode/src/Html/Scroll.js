b8e48e9d2e8e6ada313c5b99c64432086402feb8
jonastosse <79142901+jonastosse@users.noreply.github.com>
2022-03-16 13:12:28 +0100

    A big change on the landing page.

--- Bikerental_Website_EasyMode/src/Html/Scroll.js
+++ Bikerental_Website_EasyMode/src/Html/Scroll.js
@@ -1,17 +1,3 @@
-$(function() {
-    $('#header-button-1').removeClass('toggled');
-
-    $('#search-icon').click(function(e) {
-        e.stopPropagation();
-        $('#header-button-1').toggleClass('toggled');
-        $("#popup-search").focus();
-    });
-
-    $('#header-button-1 input').click(function(e) {
-        e.stopPropagation();
-    });
-
-    $('#header-button-1, body').click(function() {
-        $('#search-menu').removeClass('toggled');
-    });
-});
+function myFunction(x) {
+    x.classList.toggle("change");
+}
