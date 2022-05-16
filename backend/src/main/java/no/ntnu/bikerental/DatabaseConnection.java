44f857c14113b83104edfe2b255a2a579c2eec61
Sindre Skorpen <64530905+Westdespair@users.noreply.github.com>
2022-05-16 15:15:41 +0200

    Added some clarifying comments to DatabaseConnection.java

--- backend/src/main/java/no/ntnu/bikerental/DatabaseConnection.java
+++ backend/src/main/java/no/ntnu/bikerental/DatabaseConnection.java
@@ -4,11 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Connects the SQLite database to the Springboot backend.
+ */
 public class DatabaseConnection {
     public static Connection connect() {
         Connection connection = null;
         try {
             Class.forName("org.sqlite.JDBC");
+            //Connects with the Keep Rolling Rolling Rolling database file in resources.
             connection = DriverManager.getConnection("jdbc:sqlite:KRRR_database.db");
             System.out.println("Connection success!");
         } catch (ClassNotFoundException | SQLException e) {
