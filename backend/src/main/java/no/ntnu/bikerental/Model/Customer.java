2f594c3430a82aa19ee51150b02451075b0cf2fc
Malinbsy <Malinbsy@stud.ntnu.no>
2022-05-23 13:56:08 +0200

    Implemented model classes, product, order and customer.

---
+++ backend/src/main/java/no/ntnu/bikerental/Model/Customer.java
@@ -0,0 +1,93 @@
+package no.ntnu.bikerental.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    private int customerID;
+    private String customerName;
+    private String email;
+    private String address;
+    private String locationName;
+    private int postalNumber;
+    private int privilege;
+
+
+    public Customer(int customerID, String customerName, String email, String address, String locationName, int postalNumber, int privilege){
+        this.customerID = customerID;
+        this.customerName = customerName;
+        this.email = email;
+        this.address = address;
+        this.locationName = locationName;
+        this.postalNumber = postalNumber;
+        this.privilege = privilege;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public int getPostalNumber() {
+        return postalNumber;
+    }
+
+    public int getPrivilege() {
+        return privilege;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public void setPostalNumber(int postalNumber) {
+        this.postalNumber = postalNumber;
+    }
+
+    public void setPrivilege(int privilege) {
+        this.privilege = privilege;
+    }
+}
+
+
