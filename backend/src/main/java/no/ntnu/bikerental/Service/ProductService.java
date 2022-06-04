ca9da15ba702e0c5bb85760857451c2135329dad
Malinbsy <Malinbsy@stud.ntnu.no>
2022-06-04 18:57:41 +0200

    implemented service class, customer, order and product.

---
+++ backend/src/main/java/no/ntnu/bikerental/Service/ProductService.java
@@ -0,0 +1,32 @@
+package no.ntnu.bikerental.Service;
+
+import no.ntnu.bikerental.Model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import no.ntnu.bikerental.Repository.ProductRepository;
+
+import java.util.List;
+import java.util.LinkedList;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public List<Product>  iterableToList(Iterable<Product> iterable){
+        List<Product> list = new LinkedList<>();
+        iterable.forEach(list::add);
+        return list;
+    }
+
+    public List<Product> getAllProducts(){return iterableToList(productRepository.findAll());}
+
+    public Product findOrderById(Integer id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
