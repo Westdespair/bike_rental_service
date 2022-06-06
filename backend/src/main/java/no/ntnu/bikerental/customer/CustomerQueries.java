4c16bedcc5ba95ff639ec3b7aa96b4eba7e85bd7
Sindre Skorpen <64530905+Westdespair@users.noreply.github.com>
2022-06-06 20:30:44 +0200

    Finalized functionality in customerqueries. Most fields can now be changed in the DB.

---
+++ backend/src/main/java/no/ntnu/bikerental/customer/CustomerQueries.java
@@ -0,0 +1,99 @@
+package no.ntnu.bikerental.customer;
+
+import no.ntnu.bikerental.DatabaseConnection;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CustomerQueries {
+    Connection connection = DatabaseConnection.connect();
+    Statement statement = connection.createStatement();
+
+    public CustomerQueries() throws SQLException {
+
+    }
+
+    /**
+     * Edits the name of a given user
+     * @param customerID ID of the customer whose name will be changed
+     * @param newName The new name of the customer
+     */
+    public void setName(int customerID, String newName) throws SQLException {
+        statement.executeQuery("UPDATE Customers " +
+                "SET customer_name = " + newName
+                + " WHERE customer_ID = " + customerID);
+    }
+
+    /**
+     * Sets the address, locationname, and postalnumber
+     * @param customerID The ID of the user that will be edited
+     * @param newAddress The new address
+     * @param newLocationName The new location
+     * @param newPostalNumber The new postalnumber.
+     */
+    public void setAddress(int customerID, String newAddress, String newLocationName, int newPostalNumber ) throws SQLException {
+        statement.executeQuery("UPDATE Customers "
+                + "SET address = " + newAddress
+                + ", location_name = " + newLocationName
+                + ", postal_number = " + newPostalNumber
+                + " WHERE customer_ID = " + customerID);
+    }
+
+    /**
+     * Sets the email of an user to a desired new email
+     * @param customerID ID of the user to change the email of
+     * @param newEmail The new email of the user
+     */
+    public void setEmail(int customerID, String newEmail) throws SQLException {
+        statement.executeQuery("UPDATE Customers "
+        + "SET email = " + newEmail
+        + " WHERE customer_ID = " + customerID);
+    }
+
+    /**
+     * Changes the password to a new password if the input password matches the current password.
+     * @param customerID the ID of the user to change the password of
+     * @param oldPassword the previous unencrypted password of the given user
+     * @param newPassword the new password the user wants to use
+     */
+    public void setPassword(int customerID, String oldPassword, String newPassword) throws SQLException {
+        BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
+        String newEncryptedPassword = bCrypt.encode(newPassword);
+
+        if(bCrypt.matches(oldPassword,getEncryptedPassword(customerID))) {
+            statement.executeQuery("UPDATE Customers " +
+                    "SET password = " + newEncryptedPassword + " "
+                    + "WHERE customer_ID = " + customerID);
+        }
+    }
+
+    /**
+     * If the current user is an admin, allow them to set the new role for an user
+     * @param currentUser The current user attempting to change roles
+     * @param roleChangeUser The user that will get a new role
+     * @param newRole The name of the new role
+     */
+    public void setRole(int currentUser,int roleChangeUser, String newRole) throws SQLException {
+        String currentUserRole = statement.executeQuery("SELECT role " +
+                "FROM Customers " +
+                "WHERE customer_ID = " + currentUser).getString("role");
+
+        if(currentUserRole.equalsIgnoreCase("ADMIN")) {
+            statement.executeQuery("UPDATE Customers " +
+                    "SET role = " + newRole + " "
+            + "WHERE customer_ID = " + roleChangeUser);
+        }
+    }
+
+    /**
+     *
+     * @param customerID The owner of the password
+     * @return The encrypted password as a string
+     * @throws SQLException if the SQL connectivity is broken
+     */
+    private String getEncryptedPassword(int customerID) throws SQLException {
+        return statement.executeQuery("SELECT password FROM Customers " +
+                "WHERE customer_ID = " + customerID).getString("password");
+    }
+}
--- backend/src/main/java/no/ntnu/bikerental/product/comment/CommentQueries.java
+++ backend/src/main/java/no/ntnu/bikerental/product/comment/CommentQueries.java
@@ -1,4 +1,6 @@
-package no.ntnu.bikerental;
+package no.ntnu.bikerental.product.comment;
+
+import no.ntnu.bikerental.DatabaseConnection;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -17,7 +19,7 @@ public class CommentQueries {
      * @param userID the customer_ID of the user in the database
      * @param productID the product_ID of the product the user comments on
      * @param content The comment itself.
-     * @throws SQLException
+     * @throws SQLException If there is a database connectivity error
      */
     public void addComment(int userID, int productID, String content) throws SQLException {
         statement.executeQuery("INSERT INTO Comments (content, product_ID, post_date, customer_ID) " +
@@ -32,7 +34,7 @@ public class CommentQueries {
      * @param userID the customer_ID of the user in the database
      * @param commentID the comment_ID of the current comment in the database
      * @param newContent the content that will replace the current existing content
-     * @throws SQLException
+     * @throws SQLException If there is a database connectivity error
      */
     public void editComment(int userID, int commentID, String newContent) throws SQLException {
         statement.executeQuery("UPDATE Comments "
@@ -67,7 +69,7 @@ public class CommentQueries {
      * Returns the privilege level of users. Often decides which rights they have to different functions.
      * @param customerID The ID of the user we want to check privileges for.
      * @return int privilege, 0 for banned user, 1 for user, 2 for admin
-     * @throws SQLException
+     * @throws SQLException If there is a database connectivity error
      */
     private String getUserRole(int customerID) throws SQLException {
         return statement.executeQuery("SELECT role FROM Customers " +
