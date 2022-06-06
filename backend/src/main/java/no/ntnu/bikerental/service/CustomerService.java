80a53d0d7d62e8a9204a2ef06cc985e66354ca68
Malinbsy <Malinbsy@stud.ntnu.no>
2022-06-06 17:30:44 +0200

    Implemented methods in CustomerService.

--- backend/src/main/java/no/ntnu/bikerental/service/CustomerService.java
+++ backend/src/main/java/no/ntnu/bikerental/service/CustomerService.java
@@ -1,10 +1,12 @@
 package no.ntnu.bikerental.service;
 
 import no.ntnu.bikerental.model.Customers;
+import no.ntnu.bikerental.model.Role;
 import no.ntnu.bikerental.repository.CustomersRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -12,29 +14,99 @@ public class CustomerService {
 
     private CustomersRepository customersRepository;
 
+    /**
+     * @param customersRepository
+     */
     public CustomerService(CustomersRepository customersRepository) {
         this.customersRepository = customersRepository;
     }
 
+    /**
+     * @return
+     */
     public List<Customers> getAllCustomer() {
         return (List<Customers>) customersRepository.findAll();
     }
 
+    /**
+     * @param ID
+     * @return
+     */
     public Customers findCustomerById(long ID) {
         Optional<Customers> customer = customersRepository.findById(ID);
         return customer.get();
 
     }
 
-    public String addNewCustomer(Customers customers) {
+    /**
+     * @param email
+     * @return
+     */
+    public Customers findCustomerByEmail(String email) {
+        Optional<Customers> customers = customersRepository.findByEmail(email);
+        return customers.get();
+    }
+
+    /**
+     * @param customers
+     * @return
+     */
+    public boolean addCustomer(Customers customers) {
         boolean added = false;
         if (customers != null && customers.isValid()) {
             try {
                 findCustomerById(customers.getCustomerID());
 
+            } catch (NoSuchElementException e) {
+                customers.setPassword(new BCryptPasswordEncoder().encode(customers.getPassword()));
+                customers.setRole(Role.ROLE_USER);
+                customersRepository.save(customers);
+                added = true;
             }
+        }
+        return added;
+
+    }
+
+    /**
+     * Delete a customer
+     *
+     * @param
+     * @return
+     */
+    public boolean deleteCustomer(int customerID) {
+        String errorMessage = null;
+
+        Optional<Customers> customers = customersRepository.findById((long) customerID);
+
+        if (customerID = null) {
+            errorMessage = "Customer with " + customerID + " not found.";
+        } else {
+            customers.ifPresent(value -> customersRepository.delete(value));
+        }
+
+        return customers.isPresent();
+    }
+
+    /**
+     * @param customerID
+     * @param customers
+     * @return
+     */
+    public String updateCustomer(long customerID, Customers customers) {
+        String errorMessage = null;
+        Customers existingCustomer = findCustomerById(customerID);
+        if (existingCustomer == null) {
+            errorMessage = "No customer with ID " + customerID + "exsists";
         }
 
 
+        return errorMessage;
     }
+
 }
+
+
+
+
+
