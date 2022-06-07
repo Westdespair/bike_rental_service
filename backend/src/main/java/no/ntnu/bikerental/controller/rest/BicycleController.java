38dd1f5c4d1b096b9114d9ae65cefbd637326576
Malinbsy <Malinbsy@stud.ntnu.no>
2022-06-07 04:39:34 +0200

    Class BicycleController

--- backend/src/main/java/no/ntnu/bikerental/controller/rest/BicycleController.java
+++ backend/src/main/java/no/ntnu/bikerental/controller/rest/BicycleController.java
@@ -1,4 +1,11 @@
 package no.ntnu.bikerental.controller.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class BicycleController {
+
+
 }
