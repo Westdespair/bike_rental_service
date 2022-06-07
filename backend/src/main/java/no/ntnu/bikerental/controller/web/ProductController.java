99c9e5c49007cca320496ce0cfd36dab7c3f8a52
jonastosse <79142901+jonastosse@users.noreply.github.com>
2022-06-07 11:11:02 +0200

    Bugfix

--- backend/src/main/java/no/ntnu/bikerental/controller/web/ProductController.java
+++ backend/src/main/java/no/ntnu/bikerental/controller/web/ProductController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller("ProductMapping")
 public class ProductController {
 
     /**
